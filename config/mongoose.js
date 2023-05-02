0e669ee5781b935e9bd6c4a4f02fd8f785816f83
Rohan23045 <118437754+Rohan23045@users.noreply.github.com>
2023-05-02 21:07:28 +0530

    first commit

---
+++ config/mongoose.js
@@ -0,0 +1,25 @@
+// requiring the library
+const mongoose = require("mongoose");
+
+//connecting to the database
+mongoose.connect(
+  
+ 
+  "mongodb+srv://Nitish:nitish33@cluster0.ksobj.mongodb.net/Ecommerce?retryWrites=true&w=majority"
+).then(()=>{
+  console.log("connection successful");
+}).catch((err)=>console.log("no connection"));
+
+// acquiring the connection to check if it is succesfull
+const db = mongoose.connection;
+
+// checking for the error
+db.on("error", console.error.bind(console, "error in connecting the database"));
+
+// up and running then print the statement
+db.once("open", () => {
+  console.log("succesfully connected to database");
+});
+
+// exporting the connection
+module.exports = db;
---
+++ routes/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const routers = express.Router();
+
+// user routes
+routers.use("/", require("./users"));
+
+// student routes
+routers.use("/student", require("./students"));
+
+// interview routes
+routers.use("/interview", require("./interview"));
+
+module.exports = routers;
